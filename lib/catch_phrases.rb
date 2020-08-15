46fff0b1a6f4235c0f1e0fed72dfbb57a45c06b0
Tej Charfi <charfi@usc.edu>
2020-08-15 20:24:43 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,6 +7,16 @@ end
 
 
 def toadstool
-    status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+def link 
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+def all_phrases
+  mario 
+  toadstool
+  link
 end
